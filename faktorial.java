86884ee6b20ab9fd53439ece22dc9ea3596680f3
adityaeee <94099808+adityaeee@users.noreply.github.com>
2021-12-29 18:19:07 +0700

    Add files via upload

---
+++ faktorial.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.Scanner;
+
+public class faktorial {
+
+  static int faktorial(int num) {
+    if(num == 0){
+      return 1;
+    }
+    return num * faktorial(num-1);
+  }
+  
+
+  public static void main(String[] args){
+    int bil, hasil;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Masukkan bilangan: ");
+    bil = scan.nextInt();
+    hasil = faktorial(bil);
+    System.out.println("Faktorial dari "+bil+" adalah "+hasil);
+  }
+}
